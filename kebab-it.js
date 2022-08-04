7561897e2ebca725241c8256e84c42a2547dfd6f
Andersen <rickansenn@gmail.com>
2022-08-04 19:20:48 +0800

    feat: auto kebab case codewar title

---
+++ kebab-it.js
@@ -0,0 +1,11 @@
+function convert (title) {
+    let regex = new RegExp(/[^a-z\d]/g)
+    let dirName = title.toLowerCase().replaceAll(regex, '-')
+
+    while (dirName.includes('--')) {
+       dirName = dirName.replace('--', '-')
+    }
+    return dirName
+}
+
+console.log(convert('asd asd - 123'))
