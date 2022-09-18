e104880a5749f3302b286826e15beb51bfc1e5f5
Andersen <rickansenn@gmail.com>
2022-09-18 11:36:35 +0800

    First Version: Incomplete - Open

---
+++ 5kyu/calculating-with-functions/solutions.js
@@ -0,0 +1,45 @@
+function zero(a) {
+  let str = 0 + `${a}`.slice(0, 2);
+  return str;
+}
+function one(a) {
+  let str = 1 + `${a}`.slice(0, 2);
+  return str;
+}
+function two(a) {
+  let str = 2 + `${a}`.slice(0, 2);
+  return str;
+}
+function three(a) {
+  let str = 3 + `${a}`.slice(0, 2);
+  return str;
+}
+function four(a) {
+  let str = 4 + `${a}`.slice(0, 2);
+  return str;
+}
+function five(a) {
+  let str = 5 + `${a}`.slice(0, 2);
+  return str;
+}
+function six(a) {
+  let str = 6 + `${a}`.slice(0, 2);
+  return str;
+}
+function seven(a) {
+  let str = 7 + `${a}`.slice(0, 2);
+  return str;
+}
+function eight(a) {
+  let str = 8 + `${a}`.slice(0, 2);
+  return str;
+}
+function nine(a) {
+  let str = 9 + `${a}`.slice(0, 2);
+  return str;
+}
+
+const plus = (a) => `+${a}`;
+const minus = (a) => `-${a}`;
+const times = (a) => `*${a}`;
+const dividedBy = (a) => `/${a}`;
