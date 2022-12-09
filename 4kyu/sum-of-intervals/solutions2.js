48144194d3ddc49e2269e7ebbfb90e415bb20d41
Andersen <rickansenn@gmail.com>
2022-12-09 23:12:53 +0800

    Incomplete: Different solution

--- 4kyu/sum-of-intervals/solutions2.js
+++ 4kyu/sum-of-intervals/solutions2.js
@@ -1,37 +1,44 @@
 function sumIntervals(intervals) {
     let sorted = intervals.sort((a,b) => a[0] - b[0] || a[1] - b[1])
-    let sortedDupe = sorted.slice()
-    let arr = []
+    let sortedDupe = sorted.slice(0)
+    let pairs = []
+    // for (let i = 0; i < intervals.length - 1; i++) {
+    //     if (sortedDupe[0][1] >= sortedDupe[1][0]) {
+    //         let splice = sortedDupe.splice(0,2)
+    //         merged.push([splice[0][0], splice[1][1]])
+    //         i++
+    //     } else {
+    //         let splice = sortedDupe.splice(0,1)
+    //         merged.push(splice)
+    //     }
+    // }
 
-        for ( let i = 0; i < sorted.length -1; i++) {
-            if(sortedDupe.length > 1) {
-                if (sortedDupe[0][1] >= sortedDupe[1][0]) {
-                    let sliced = sortedDupe.splice(0,2)
-                    arr.push([sliced[0][0], sliced[1][1]])
+   for (let i = 0; i < intervals.length / 2; i++) pairs.push(sorted.slice(i*2, i*2+2)) 
+
+while (!isOverlapping) {
+            for (let i = 0; i < pairs.length; i++) {
+            if (i !== pairs.length - 1){
+                if (pairs[i][0][1] >= pairs[i][1][0]) {
+                    pairs[i] = mergeArr(pairs[i][0], pairs[i][1])
                     continue
-                } else {
-                    let sliced = sortedDupe.splice(0,1)
-                    arr.push(...sliced)
-                    continue
-                }
-            } 
-            let sliced = sortedDupe.splice(0,1)
-            arr.push(...sliced)
+                } 
+            }  
+            if (pairs[i].length === 1) pairs[i] = pairs[i][0]
         }
 
-    return arr
-  
+    pairs = Array(Math.ceil(pairs.length / 2)).fill('').map( (x,y) => pairs.slice(y*2, y*2+2))
 }
 
-   
-    
 
+
+
+
+
+    return pairs
+}
 
 const mergeArr = (arr1, arr2) => [arr1[0], arr2[1]]
-const isOverlapping = arr => {
-    return arr.map((x,y,z) => y !== z.length -1 ? x[1] >= z[y+1][0] : x[0] <= z[y-1][1]).some(x => x === true)
-}
-// console.log(isOverlapping([ [1,2], [3,4], [5,6], [6,7]]))
+const isOverlapping = arr => arr.map((x,y,z) => y !== z.length -1 ? x[1] >= z[y+1][0] : x[0] <= z[y-1][1]).every(x => x === false)
 
 
 console.log(
