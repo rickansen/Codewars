acf30fae91273684ec5998699ab2f57332c0c185
Andersen <rickansenn@gmail.com>
2023-01-18 20:58:10 +0800

    First Version: Closed

---
+++ 8kyu/return-the-day/solutions.js
@@ -0,0 +1,11 @@
+function whatday(num) { 
+    if (num === 1) return "Sunday"
+    if (num === 2) return "Monday"
+    if (num === 3) return "Tuesday"
+    if (num === 4) return "Wednesday"
+    if (num === 5) return "Thursday"
+    if (num === 6) return "Friday"
+    if (num === 7) return "Saturday"
+    
+    return "Wrong, please enter a number between 1 and 7"
+}
