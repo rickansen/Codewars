723bede9c8a1be684beb7478b819d057bceffda9
Andersen <rickansenn@gmail.com>
2023-02-01 17:20:29 +0800

    First Version: Closed

---
+++ 8kyu/training-js-10-loop-statement-for/solutions.js
@@ -0,0 +1,13 @@
+function pickIt(arr){
+    var odd=[],even=[];
+    
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 1) {
+        odd.push(arr[i])
+      } else {
+        even.push(arr[i])
+      }
+    }
+  
+    return [odd,even];
+  }
