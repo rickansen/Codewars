de040e967a698fed1d59e77381a46d12251b48e5
Andersen <rickansenn@gmail.com>
2023-02-19 22:55:04 +0800

    First Version: Closed

---
+++ 8kyu/find-out-whether-the-shape-is-a-cube/solutions.js
@@ -0,0 +1,11 @@
+var cubeChecker = (volume, side) => volume > 0 ? side ** 3 === volume : false
+
+console.log(cubeChecker(56.3, 1), false);
+console.log(cubeChecker(-1, 2), false);
+console.log(cubeChecker(8, 3), false);
+console.log(cubeChecker(8, 2), true);
+console.log(cubeChecker(-8,-2), false);
+console.log(cubeChecker(0, 0), false);
+console.log(cubeChecker(1, 5),  false);
+console.log(cubeChecker(125, 5), true);
+console.log(cubeChecker(125,-5), false);
