5c0aa70f4e6aa98c7973d648eef965d1e79d3e52
Andersen <rickansenn@gmail.com>
2023-04-07 09:41:00 +0800

    First Version: Closed

---
+++ 8kyu/get-nth-even-number/solutions.js
@@ -0,0 +1,9 @@
+const nthEven = n => n * 2 - 2
+
+
+console.log(nthEven(1), 0, "Wrong Value!");
+console.log(nthEven(2), 2, "Wrong Value!");
+console.log(nthEven(3), 4, "Wrong Value!");
+
+console.log(nthEven(100), 198, "Wrong Value!");
+console.log(nthEven(1298734), 2597466, "Wrong Value!");
