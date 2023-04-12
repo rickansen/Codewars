9d8d1efc2222607f774f0a3c56298bc1d1feb6a8
Andersen <rickansenn@gmail.com>
2023-04-12 10:06:33 +0800

    First Version: Closed

---
+++ 8kyu/find-the-position/solutions.js
@@ -0,0 +1,6 @@
+const position = letter => `Position of alphabet: ${String(letter).charCodeAt() - 96}`
+
+
+console.log(position("a"),"Position of alphabet: 1");
+console.log(position("z"),"Position of alphabet: 26");
+console.log(position("e"),"Position of alphabet: 5");
