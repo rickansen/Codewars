da3b2a691fdd3d7f1afe9ed1c0bbacdf347881f4
Andersen <rickansenn@gmail.com>
2023-04-30 15:01:25 +0800

    First Version: Closed

---
+++ 8kyu/alan-partridge-ii-apple-turnover/solutions.js
@@ -0,0 +1,5 @@
+const apple = x => Number(x) ** 2 > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+
+
+console.log(apple('50'), 'It\'s hotter than the sun!!'); 
+console.log(apple(4), 'Help yourself to a honeycomb Yorkie for the glovebox.'); 
