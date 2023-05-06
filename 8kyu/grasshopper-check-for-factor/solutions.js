383db02c416bfb8f1c5713f0f2974c09d01606f2
Andersen <rickansenn@gmail.com>
2023-05-06 20:47:18 +0800

    First Version: Closed

---
+++ 8kyu/grasshopper-check-for-factor/solutions.js
@@ -0,0 +1,6 @@
+const checkForFactor = (base, factor) => base % factor === 0
+
+console.log(checkForFactor(10,2), true)
+console.log(checkForFactor(63,7), true)
+console.log(checkForFactor(2450,5), true)
+console.log(checkForFactor(24612,3), true)
