9aff5b72157d0cf2ae4c003419cdd6bc6d91e8c0
Andersen <rickansenn@gmail.com>
2023-05-07 20:50:54 +0800

    First Version: Closed

---
+++ 8kyu/volume-of-a-cuboid/solutions.js
@@ -0,0 +1,8 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+}
+
+console.log(Kata.getVolumeOfCuboid(1,2,2),  4);
+console.log(Kata.getVolumeOfCuboid(6,2,5), 60);
