c385c9c0764899c95565773313feb441df69574e
Andersen <rickansenn@gmail.com>
2023-05-10 21:17:18 +0800

    First Version: Closed

---
+++ 8kyu/short-long-short/solutions.js
@@ -0,0 +1,6 @@
+const solution = (a, b) => a.length > b.length ? b+a+b : a+b+a
+
+console.log(solution('45', '1'), '1451');
+console.log(solution('13', '200'), '1320013');
+console.log(solution('Soon', 'Me'), 'MeSoonMe');
+console.log(solution('U', 'False'), 'UFalseU');
