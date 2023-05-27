0841039cb9857349c28cd06bb70952e058f5dbc1
Andersen <rickansenn@gmail.com>
2023-05-27 22:24:07 +0800

    Second Version: Updated

--- 7kyu/no-oddities-here/solutions.js
+++ 7kyu/no-oddities-here/solutions.js
@@ -1,4 +1,4 @@
-const noOdds = values => values.filter( x => x % 2 === 0)
+const noOdds = values => values.filter(x => x % 2 === 0)
 
 console.log( noOdds( [0,1] ), [0] )
 console.log( noOdds( [0,1,2,3] ), [0,2] )
